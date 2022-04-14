5172a24be8b0c934b2359f9732f52f2ce6f6f64d
NSarkisyan <NSarkisyan@lanit.ru>
2022-04-14 14:53:31 +0300

    for Sergey

---
+++ src/main/java/com/nareksarkisyan/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.nareksarkisyan.spring.mvc_hibernate_aop.service;
+
+import com.nareksarkisyan.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+     public List<Employee> getAllEmployees();
+     public void saveEmployee(Employee employee);
+     public Employee getEmployee(int id);
+
+     public void deleteEmployee(Employee employee);
+}
